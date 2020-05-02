1d06875b20aeab4196c65a81e6062c4a66e381ee
arjenkins <arobinsonjenkins@gmail.com>
2020-05-01 18:00:26 -0700

    rendered order view

--- src/screens/ViewOrders.js
+++ src/screens/ViewOrders.js
@@ -1,14 +1,36 @@
 import React, { useContext } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Button, FlatList } from 'react-native';
 import MealContext from '../context/Context';
 
 const ViewOrdersScreen = props => {
   const { state, removeOrder } = useContext(MealContext);
-  const orders = state.orders;
+ // const meal = state.meals.find(meal => meal.id === props.route.params.mealId);
 
+  const orders = state.orders;
+  
+  console.log('HERE IS THE ORDER INFO', orders);
+  console.log('the digits', );
   return (
     <View style={styles.container}>
-      <Text>Add an Order List to me!</Text>
+     <Text>Add an Order List to me!</Text>
+  
+<FlatList
+  data={orders}
+  renderItem={itemData => {
+    console.log('the new info', itemData);
+    return (
+      <View style={styles.infoCard}>
+        <Text>{itemData.item.meal.title}</Text>
+        <Text>{itemData.item.meal.price}</Text>
+        <Button title='Delete Order' onPress={() => {
+     removeOrder(itemData.item.id)}
+   } 
+   />
+      </View>
+      
+   )
+  }}
+  />
     </View>
   );
 };
@@ -16,10 +38,23 @@ const ViewOrdersScreen = props => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    margin: 10,
+    margin: 30,
+    borderRadius: 10,
+    backgroundColor: 'rgb(255, 255, 255)',
     justifyContent: 'center',
     alignItems: 'center',
   },
+  infoCard: {
+    flex: 1,
+    height: 100,
+    width: 90,
+    margin: 30,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#999',
+    borderRadius: 5,
+  },
+
 });
 
 export default ViewOrdersScreen;
